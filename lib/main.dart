73d0a3db8a27440b54035a9ffcd34fb9203b398c
LahiruMS <lahiru96dreamz@gmail.com>
2024-10-23 14:37:13 +0530

    Initial commit - adding existing project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dice_roller/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    );
+  }
+}
